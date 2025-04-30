279d511a151e25e88b76b5b7b9a03b34da863258
iragca <irag.chrisandrei@mailbox.org>
2025-04-30 17:18:05 +0800

    Add data preprocessing script for Civil Service Commission dataset
    - Implemented a new script `preprocess.py` to load, format, and save the Civil Service Commission data.
    - Utilized DuckDB to read data from a database and Polars for data manipulation.
    - Converted "Posting Date" and "Closing Date" columns to datetime format.
    - Cast "Action" column to integer type.
    - Saved the processed data to a CSV file in the specified directory.
    - Added logging for data loading, conversion, and saving processes.

---
+++ ml_final_project/preprocess.py
@@ -0,0 +1,54 @@
+import duckdb
+import polars as pl
+
+from ml_final_project.config import RAW_DATA_DIR, PROCESSED_DATA_DIR, logger
+
+if __name__ == "__main__":
+
+    try:
+        # Load the data as a polars DataFrame
+        dbPath = (
+            RAW_DATA_DIR
+            / "CivilServiceCommission"
+            / "civilservicecommission.duckdb"
+        )
+        db = duckdb.connect(dbPath, read_only=True)
+        data = db.sql("SELECT * FROM civilservicecommission").pl()
+
+        logger.info(
+            f"Loaded {data.shape[0]} rows and {data.shape[1]} columns from the database."
+        )
+        logger.info(f"Data sample:\n{data.head(2)}")
+
+        # Convert the "Posting Date" and "Closing Date" columns to datetime
+        formatted_data = data.with_columns(
+            [
+                pl.col("Posting Date")
+                .str.strptime(
+                    pl.Date, "%d %b %Y"
+                )  # Convert from "20 Apr 2025"
+                .alias("Posting Date"),
+                pl.col("Closing Date")
+                .str.strptime(
+                    pl.Date, "%d %b %Y"
+                )  # Convert from "20 Apr 2025"
+                .alias("Closing Date"),
+                pl.col("Action").cast(pl.Int32).alias("Action"),
+            ]
+        )
+
+        logger.info(f"Converted data sample:\n{formatted_data.head(2)}")
+
+        # Save the formatted data to a CSV file
+        formatted_data.write_csv(
+            PROCESSED_DATA_DIR / "civilservicecommission.csv"
+        )
+
+        logger.success(
+            f"Saved the formatted data to {PROCESSED_DATA_DIR / 'civilservicecommission.csv'}."
+        )
+    except Exception as e:
+        logger.error(f"An error occurred: {e}")
+    finally:
+        db.close()
+        logger.info("Closed the database connection.")
