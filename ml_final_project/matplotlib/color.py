83b4f1897f03fa46c6a10784479380a3e77f3219
iragca <irag.chrisandrei@mailbox.org>
2025-05-04 08:11:51 +0800

    add more colors

--- ml_final_project/matplotlib/color.py
+++ ml_final_project/matplotlib/color.py
@@ -5,10 +5,11 @@ from ml_final_project.config import logger
 class Color(Enum):
     """Enum for colors used in the project.
 
-    Inspired by the accessible color palette from
+    Primary colors are inspired by the accessible color palette from
     https://github.com/mpetroff/accessible-color-cycles
     """
 
+    # Primary
     BLUE = "#5790fc"
     ORANGE = "#f89c20"
     RED = "#e42536"
@@ -16,11 +17,22 @@ class Color(Enum):
     GRAY = "#9c9ca1"
     VIOLET = "#7a21dd"
 
+    # Secondary
+    DARK_GRAY = "#3b3b3b"
+    LIGHT_GRAY = "#BFBFBF"
 
-color_str = "\n"
-for color in Color:
-    color_str += f"{color.name}: {color.value}\n"
+    DARK_BLUE = "#072ea5"
+    LIGHT_BLUE = "#91c2ed"
+
+    DARK_ORANGE = "#ff3e06"
+    LIGHT_ORANGE = "#fbb181"
+
+    DARK_RED = "#a50000"
+    LIGHT_RED = "#ea6466"
+
+    SOFT_BLACK = "#333333"
+
 
 logger.info(
-    f"Accessible colors by https://github.com/mpetroff/accessible-color-cycles. {color_str}",
+    "Accessible color palette inspired by https://github.com/mpetroff/accessible-color-cycles."
 )
