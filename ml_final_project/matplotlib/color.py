e73152b72e2ea517f61fe82073ff6cddb46129af
iragca <irag.chrisandrei@mailbox.org>
2025-05-09 11:56:32 +0800

    added color palettes

--- ml_final_project/matplotlib/color.py
+++ ml_final_project/matplotlib/color.py
@@ -1,4 +1,7 @@
 from enum import Enum
+
+import matplotlib.colors as mcolors
+
 from ml_final_project.config import logger
 
 
@@ -16,10 +19,12 @@ class Color(Enum):
     PURPLE = "#964a8b"
     GRAY = "#9c9ca1"
     VIOLET = "#7a21dd"
+    WHITE = "#ffffff"
 
     # Secondary
     DARK_GRAY = "#3b3b3b"
     LIGHT_GRAY = "#BFBFBF"
+    PUSH_TO_BG = "#dddddd"
 
     DARK_BLUE = "#072ea5"
     LIGHT_BLUE = "#91c2ed"
@@ -31,6 +36,74 @@ class Color(Enum):
     LIGHT_RED = "#ea6466"
 
     SOFT_BLACK = "#333333"
+
+    @classmethod
+    def get_primary_colors(cls):
+        """Get primary colors as a list."""
+        return [
+            cls.BLUE.value,
+            cls.ORANGE.value,
+            cls.RED.value,
+            cls.PURPLE.value,
+            cls.GRAY.value,
+            cls.VIOLET.value,
+        ]
+
+    @classmethod
+    def get_secondary_colors(cls):
+        """Get secondary colors as a list."""
+        return [
+            cls.DARK_GRAY.value,
+            cls.LIGHT_GRAY.value,
+            cls.DARK_BLUE.value,
+            cls.LIGHT_BLUE.value,
+            cls.DARK_ORANGE.value,
+            cls.LIGHT_ORANGE.value,
+            cls.DARK_RED.value,
+            cls.LIGHT_RED.value,
+            cls.SOFT_BLACK.value,
+        ]
+
+    @classmethod
+    def get_all_colors(cls: type) -> list:
+        """Get all colors as a list."""
+        pallete = cls.get_primary_colors() + cls.get_secondary_colors()
+        return pallete
+
+    @classmethod
+    def BlWhOr(cls: type) -> mcolors.LinearSegmentedColormap:
+        """Get a color palette for B/W."""
+        pallete: list = [
+            cls.BLUE.value,
+            cls.WHITE.value,
+            cls.ORANGE.value,
+        ]
+        return mcolors.LinearSegmentedColormap.from_list(
+            "BlWhOr", pallete, N=256
+        )
+
+    @classmethod
+    def BlWhRd(cls: type) -> mcolors.LinearSegmentedColormap:
+        """Get a color palette for B/W."""
+        pallete: list = [
+            cls.BLUE.value,
+            cls.WHITE.value,
+            cls.RED.value,
+        ]
+        return mcolors.LinearSegmentedColormap.from_list(
+            "BlWhRd", pallete, N=256
+        )
+
+    @classmethod
+    def WhBl(cls: type) -> mcolors.LinearSegmentedColormap:
+        """Get a color palette for B/W."""
+        palette: list = [
+            cls.WHITE.value,
+            cls.BLUE.value,
+        ]
+        return mcolors.LinearSegmentedColormap.from_list(
+            "BlWhOr", palette, N=256
+        )
 
 
 logger.info(
